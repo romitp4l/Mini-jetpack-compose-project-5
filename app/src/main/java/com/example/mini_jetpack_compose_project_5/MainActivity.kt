0c7983f0b8f9d04a28e0bfbef0acfc8852be9c17
Romit Pal <romitp4l@Romit-Pal-s-weapon.local>
2024-03-28 16:09:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mini_jetpack_compose_project_5/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.mini_jetpack_compose_project_5
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.mini_jetpack_compose_project_5.ui.theme.Minijetpackcomposeproject5Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Minijetpackcomposeproject5Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Minijetpackcomposeproject5Theme {
+        Greeting("Android")
+    }
+}
